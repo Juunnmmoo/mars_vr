30c34d6fd39619c285f2da4b77e7002498d6b09c
aruel7714 <harunia7714@daum.net>
2022-08-13 20:38:08 +0900

    tutorial.cs 수정
    코루틴 추가
    script1 함수에 switch~case 추가

--- MarsVR/Assets/2. Scripts/Tutorial.cs
+++ MarsVR/Assets/2. Scripts/Tutorial.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    CupCtrl cupctrl;
-    PlayerCtrl playerctrl;
-    List<string> scriptList = new List<string>();
+    private CupCtrl cupctrl;
+    private PlayerCtrl playerctrl;
+    private BottleCtrl bottlectrl1;
+    private BottleCtrl bottlectrl2;
+    private BottleCtrl bottlectrl3;
+    private List<string> scriptList = new List<string>();
 
-    int TutorialNum = -1;
+    private int tutorialNum = 0;
+
+    private bool check;
+
+    [Range(1.5f, 3.0f)]
+    public float nextTime = 2.0f;
 
     [Header("UI 관련")]
     //scriptListUI
@@ -20,32 +28,103 @@ public class Tutorial : MonoBehaviour
     {
         cupctrl = GameObject.Find("Cup").GetComponent<CupCtrl>();
         playerctrl = GameObject.Find("Player").GetComponent<PlayerCtrl>();
+        bottlectrl1 = GameObject.Find("Bottle").GetComponent<BottleCtrl>();
+        bottlectrl2 = GameObject.Find("Bottle (1)").GetComponent<BottleCtrl>();
+        bottlectrl3 = GameObject.Find("Bottle (2)").GetComponent<BottleCtrl>();
 
         scriptList = FileIO.ReadScript();
-        foreach (string str in scriptList)
+        for (int i = 0; i < scriptList.Count; i++)
         {
-            Debug.LogWarning(str);
+            Debug.LogWarning(i + "=" + scriptList[i]);
+
         }
 
-        scriptChange();
-        InvokeRepeating("script1", 1.5f, 3.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        script1();
     }
 
-    void script1() {
-        if(TutorialNum<4)
-            gameObject.GetComponentInChildren<Text>().text = scriptList[TutorialNum++];
+    // 튜토리얼 스크립트 진행 함수
+    void script1()
+    {
+        switch (tutorialNum)
+        {
+            case 0:
+            case 1:
+            case 2:
+                NextScript();
+                break;
+            case 3:
+                // 컵을 집을때
+                if (cupctrl.isUsing)
+                    NextScript();
+                break;
+            case 4:
+            case 5:
+                NextScript();
+                break;
+            case 6:
+                // 병을 집을때
+                if (bottlectrl1.isUsing)
+                    NextScript();
+                break;
+            case 7:
+            case 8:
+                NextScript();
+                break;
+            case 9:
+                // 레몬즙 20ml 넣었을때
+                if(((int)cupctrl.amounts[cupctrl.amounts.Count - 1]) >= 20)
+                {
+                    NextScript();
+                }
+                break;
+            case 10:
+            case 11:
+            case 12:
+                NextScript();
+                break;
+            case 13:
+                // 데킬라 20ml, 화이트럼 20ml 넣었을때
+            case 14:
+                
+            default:
+                break;
+        }
+        gameObject.GetComponentInChildren<Text>().text = scriptList[tutorialNum];
+
     }
 
     //ScriptUI 바꿔줌 
     private void scriptChange()
     {
-           
+
     }
+
+    private IEnumerator NextScriptCor(float time)
+    {
+        float elaspedTime = 0;      // 경과 시간
+        while (elaspedTime < time)
+        {
+            elaspedTime += Time.deltaTime;
+            yield return null;
+        }
+        check = false;
+        tutorialNum++;
+    }
+
+    void NextScript()
+    {
+        if (check == true)
+            return;
+        check = true;
+        StartCoroutine(NextScriptCor(nextTime));
+
+    }
+
 
 }
